798811ca973d044975fd80bc326063f77d663d98
Grid Alpha <79061262+griz2d33@users.noreply.github.com>
2023-01-09 09:48:57 -0800

    Add files via upload

--- assets/scripts/core.js
+++ assets/scripts/core.js
@@ -1,1 +1,8 @@
 console.log("Hello World");
+
+startButton = document.querySelector(".get-start");
+mailDialog = document.querySelector("#dialog");
+
+addEventListener("click",()=>{
+    mailDialog.showModal();
+});
